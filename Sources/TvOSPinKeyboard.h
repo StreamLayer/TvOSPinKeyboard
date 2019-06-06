884d7ac31c291d5d6dac5a1b1d440657ccb52948
AVVS <v@makeomatic.ru>
2019-06-06 12:05:34 -0700

    chore: add build settings

---
+++ Sources/TvOSPinKeyboard.h
@@ -0,0 +1,17 @@
+//
+//  TvOSPinKeyboard.h
+//  TvOSPinKeyboard
+//
+//  Created by Vitaly Aminev on 2019-06-04.
+//  Copyright © 2019 StreamLayer, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TvOSPinKeyboard.
+FOUNDATION_EXPORT double TvOSPinKeyboardVersionNumber;
+
+//! Project version string for TvOSPinKeyboard.
+FOUNDATION_EXPORT const unsigned char TvOSPinKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TvOSPinKeyboard/PublicHeader.h>
