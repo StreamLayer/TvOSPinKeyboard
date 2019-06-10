03cece29152bce9d79abc8b2a6e579146a41b724
AVVS <v@makeomatic.ru>
2019-06-09 22:23:28 -0700

    chore: swift 5, staticlib

--- Carthage/Checkouts/FocusTvButton/FocusTvButton/FocusTvButton/FocusTvButton.h
+++ Carthage/Checkouts/FocusTvButton/FocusTvButton/FocusTvButton/FocusTvButton.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double FocusTvButtonVersionNumber;
 FOUNDATION_EXPORT const unsigned char FocusTvButtonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FocusTvButton/PublicHeader.h>
-
-
