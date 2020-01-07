89393f6ccf15c310752c7de45e703281b067325b
AVVS <v@makeomatic.ca>
2020-01-06 19:20:44 -0800

    fix: update cartography dep

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "TvOSPinKeyboard",
+    platforms: [
+      .tvOS(.v11)
+    ],
     products: [
         .library(name: "TvOSPinKeyboard", targets: ["TvOSPinKeyboard"])
     ],
