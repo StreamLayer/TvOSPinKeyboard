d80c86c6bca4c2d6cc62e2e8777e969109c177cc
AVVS <v@makeomatic.ca>
2020-01-13 17:38:21 -0800

    fix: try with xcodegen

--- Sources/TvOSPinKeyboard.h
+++ Sources/TvOSPinKeyboard.h
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for TvOSPinKeyboard.
-FOUNDATION_EXPORT double TvOSPinKeyboardVersionNumber;
+FOUNDATION_EXPORT double SLR_TvOSPinKeyboardVersionNumber;
 
 //! Project version string for TvOSPinKeyboard.
-FOUNDATION_EXPORT const unsigned char TvOSPinKeyboardVersionString[];
+FOUNDATION_EXPORT const unsigned char SLR_TvOSPinKeyboardVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TvOSPinKeyboard/PublicHeader.h>
